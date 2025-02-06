01e85f2fa3a7595a4c79a66e06fdcc3ca6b01202
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-02-06 21:13:29 +0100

    Primer commit con repositorio limpio

---
+++ src/main/java/com/KirutasuJava/SpringEjercicios01Palindromo/SpringEjercicios01PalindromoApplication.java
@@ -0,0 +1,13 @@
+package com.KirutasuJava.SpringEjercicios01Palindromo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEjercicios01PalindromoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEjercicios01PalindromoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KirutasuJava/SpringEjercicios01Palindromo/SpringEjercicios01PalindromoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KirutasuJava.SpringEjercicios01Palindromo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEjercicios01PalindromoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
