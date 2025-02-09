fd1de178b12f436977d0b8b7103e30b4046a1e4f
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-02-09 13:37:46 +0100

    Comentarios añadidos

--- src/main/java/com/KirutasuJava/SpringEjercicios01Palindromo/controller/PalindromoController.java
+++ src/main/java/com/KirutasuJava/SpringEjercicios01Palindromo/controller/PalindromoController.java
@@ -4,9 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controlador para verificar palindromos
+ */
 @RestController
 public class PalindromoController {
 
+    /**
+     * Endpoint para comprobar si una palabra es un palindromo
+     * @param palabra la palabra a verificar
+     * @return mensaje indicando si la palabra es un palindromo o no
+     */
     @GetMapping ("/validar-palindromo/{palabra}") // TODO Controlador solo comunica peticiones http y devuelve respuesta, no debe llevar la logica
     public String Palindromo(@PathVariable String palabra) {
         if (esPalindromo(palabra)) {
@@ -17,6 +25,11 @@ public class PalindromoController {
         }
     }
 
+    /**
+     * Metodo para comprobar si una palabra es un palindromo
+     * @param palabra la palabra a comprobar
+     * @return true si la palabra es un palindromo; false si no lo es.
+     */
     private boolean esPalindromo(String palabra) { //todo este metodo iria en su clase independiente, y NO en el controlador, pero es solo para probar el funcionamiento de Spring Boot
         int lenght = palabra.length();
         for (int i = 0; i < lenght / 2; i++) {
